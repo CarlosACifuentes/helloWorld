d6be98ef7d0d6e9933554e032cd703860168012e
Carlos Cifuentes <cabetocifuentes@gmail.com>
2021-09-10 16:42:01 -0400

    viewController changes.

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -8,7 +8,10 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var label: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +29,10 @@ class ViewController: NSViewController {
         return num1 + num2
     }
 
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        label.stringValue = "Hello \(textField.stringValue)!"
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
